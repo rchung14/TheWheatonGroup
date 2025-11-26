f94e447dc49f7e286371a617fde4fb4eb3cadc3b
Ryan Chung <ryanchung@Ryans-MacBook-Pro-2.local>
2025-11-25 23:13:23 -0500

    render troubleshoot

--- server/database/firebaseConfig.js
+++ server/database/firebaseConfig.js
@@ -4,14 +4,23 @@ import fs from "fs";
 
 dotenv.config();
 
-// Load the service account JSON using fs instead of assert
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
-);
+let serviceAccount;
+
+// Load service account from environment (Render)
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // Local development loads file
+  serviceAccount = JSON.parse(
+    fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://wheatongroup-f7e34-default-rtdb.firebaseio.com",
+  databaseURL:
+    process.env.FIREBASE_DATABASE_URL ||
+    "https://wheatongroup-f7e34-default-rtdb.firebaseio.com",
 });
 
 const database = admin.database();
